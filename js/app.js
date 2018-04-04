ffa0aed8e88f9cf679416d3159c7b9571f067430
brendamesias <brendamesiasvillar@gmail.com>
2018-04-04 11:23:34 -0500

    cambios

--- js/app.js
+++ js/app.js
@@ -1,96 +1,169 @@
-$(document).ready(function() {
+let data = [
+      {
+          "nombre":"Arduino Day 2018",
+          "fecha": "sábado, 7 de abril", 
+          "hora": "( 9:00 a 17:00 )", 
+          "costo": "Gratis",
+          "ubicacion": "Auditorio Rosa Alarco Larrabure - UNMSM", 
+          "descripcion": "Evento mundial que celebra el desarrollo de soluciones tecnologicas en Open Hardware. La tecnologia Arduino es la lider indiscutible en implementar aplicaciones Hardware y Software. En el Peru la sede del evento mundial es la Universidad Nacional Mayor de San Marcos y esa organizado por el grupo de investigacion de Internet de las Cosas.      Ingreso gratuito, capacidad limitada https://docs.google.com/forms/d/e/1FAIpQLSeD3NM-4cOuqUiVckOfp5lAa52ae3e2plvxLtEzZx7gvceYuw/viewform", 
+          "imagen": "Arduino-Day.jpg",
+          "tipo":"Tecnologico"
+      },
+      {
+          "nombre":"Scrum Day Perú 2018",
+          "fecha": "27 de abril - 28 de abril", 
+          "hora": "(9:00 a 18:00)", 
+          "costo": "S/. 500.00",
+          "ubicacion": "Sol de Oro Hotel & Suites, Calle San Martin 305, Miraflores, Lima 18", 
+          "descripcion": "El Scrum Day Perú es la conferencia anual que reúne en Lima a los mejores exponentes de la agilidad en Iberoamérica. En esta edición esperamos reunir a más de 400 agilistas, 39 charlas, 30 speakers, 1 open space y 7 países representados en un evento que dará que hablar en la región. Entradas en ➡ http://sdp2018.scrumdayperu.org/index.php/producto/entrada-general/ Incluye coffee breaks a media mañana y media tarde. Programa de actividades : https://www.facebook.com/events/2002674699989679/", 
+          "imagen": "scrum-day.jpg",
+          "tipo":"Empresarial"
+      },
+      {
+          "nombre":"Taller gratuito: Kanban para emprendedores en La Muralla",
+          "fecha": "jueves, 5 de abril", 
+          "hora": "( 19:00 a 21:00 )", 
+          "costo": "Gratis",
+          "ubicacion": "Parque de La Muralla, Jirón Amzonas, 01 Lima", 
+          "descripcion": "DIRIGIDO A: Todas las personas que quieren aprender el método Kanban para usarlo en sus emprendimientos, proyecto, centro de labores, su vida diaria, o que se quieran formar como facilitadores. Objetivo: Dar un overview de este gran método en un taller 90% práctico. Entrada: libre Certificado S/. 20.00 Duración: 02 horas cronológicas Informacíon:https://goo.gl/forms/eNawrTya1Q2bYxn13", 
+          "imagen": "kanb-emprende.jpg",
+          "tipo":"Empresarial"
+      },
+      {
+          "nombre":"SpaceUp Peru",
+          "fecha": "sábado, 14 de abril", 
+          "hora": "( 9:00 a 18:30 )", 
+          "costo": "Gratis",
+          "ubicacion": "UTEC - Universidad de Ingeniería y Tecnología", 
+          "descripcion": "SpaceUp Peru es una no-conferencia, donde todos están invitados a hablar sobre temas relacionados con el sector espacial, sin importar su background o edad. Es la primera ocasión en que se realizará un SpaceUp en Peru, y la tercera en Latinoamérica, Ingreso gratuito, capacidad limitada. https://www.facebook.com/events/316142072240159/?active_tab=about", 
+          "imagen": "space-up.jpg",
+          "tipo":"Tecnologico"
+      },
+      {
+          "nombre":"Congreso Nacional Estudiantil de Gestión de Proyectos (CONEGP)",
+          "fecha": "15 - 18 de agosto", 
+          "hora": "9:00 (15 de agosto) - 21:00 (18 de agosto)", 
+          "costo": "Gratis",
+          "ubicacion": "Avenida Juan Pablo II 306, 07011 Bellavista, Perú", 
+          "descripcion": "Somos un equipo de estudiantes que promueve el desarrollo del CONEGP; un evento académico importante que brinda herramientas, habilidades blandas y el uso de la tecnología como brazo estratégico en la formación integral de la Dirección de Proyectos. Ingreso gratuito. https://www.facebook.com/events/1860995150577735/", 
+          "imagen": "conegp.jpg",
+          "tipo":"Empresarial"
+      },
+      {
+          "nombre":"2da Maratón tecnológica Innovatec",
+          "fecha": "10 y 11 de Abril / 21 y 22 de Abril", 
+          "hora": "( 18:00 a 21:00 )", 
+          "costo": "Gratis",
+          "ubicacion": "Muninet-Parque de la Muralla", 
+          "descripcion": "Desde HOY inscríbete a nuestra II Maratón Tecnológica Innov@tec, donde se busca desarrollar soluciones innovadoras y tecnológicas entorno a los siguientes temas: Desnutrición infantil Violencia de género Medio Ambiente Ingreso gratuito, capacidad limitada. http://bit.ly/2HEpJs8", 
+          "imagen": "maraton-innovatec.jpg",
+          "tipo":"Tecnologico"
+      },
+      {
+          "nombre":"Programación PLC para procesos industriales",
+          "fecha": "Martes 10 de abril", 
+          "hora": "( 20:00 a 22:00 )", 
+          "costo": "Gratis",
+          "ubicacion": "German Schreiber 276, 0511 Lima", 
+          "descripcion": "Certificado opcional y acceso a la grabación aquí ➡ https://goo.gl/RGsg2i 100 vacantes libres aquí ➡ http://blog.capacitate.pe/plc-para-procesos-industriales-conferencia-online/ Mayor información enhttps://www.facebook.com/events/197281274194549/", 
+          "imagen": "prog-plc.jpg",
+          "tipo":"Tecnologico"
+      },
+      {
+          "nombre":"Seminario: Internet de las cosas (IoT) Aplicaciones Presentes y Futuras",
+          "fecha": "22 de Mayo", 
+          "hora": "( 19:00- 21:00 )", 
+          "costo": "Gratis",
+          "ubicacion": "Centro de Convenciones Internacionales INICTEL-UNI, Av. San Luis 1771 - San Borja", 
+          "descripcion": "DIRIGIDO A: Profesionales de diseño e implementación de TICs, usuarios directos,estudiantes y desarrolladores de proyectos de construcción. Descripción del Seminario:  Este Seminario de dos horas de duración presentará las aplicaciones actuales de IoT en ciudades y ámbitos rurales. Complementamos la presentación con una visión de largo alcance del IoT en la próxima década Temario ➡ https://www.facebook.com/events/358287971326838/", 
+          "imagen": "sem-iot.jpg",
+          "tipo":"Tecnologico"
+      },
+      {
+          "nombre":"Taller de Marketing Digital",
+          "fecha": "Sábado 7 de Abril", 
+          "hora": "( de 9:00 a 12:30 )", 
+          "costo": "Gratis",
+          "ubicacion": "Calle Madre de Dios 255, Santa Beatriz, lima 0 Lima", 
+          "descripcion": "El Marketing Digital está revolucionando el mundo. Aprende con nosotros a utilizar sus herramientas de manera correcta, para lograr el alcanzar tus objetivos. Capacidad limitada. https://goo.gl/forms/gtJ04MRhxnJGeSiX2", 
+          "imagen": "taller-marketing.jpg",
+          "tipo":"Empresarial"
+      },
+      {
+          "nombre":"Ideas y Estudio de Mercado",
+          "fecha": "4 y 7 de Abril", 
+          "hora": "04 de Abril ( 7:00 p.m. - 10:00 p.m.) /07 de Abril ( 10:00 a.m. - 2:00 p.m.)", 
+          "costo": "Gratis",
+          "ubicacion": "Av Javier Prado este 3502, piso 1 - San Borja", 
+          "descripcion": "Esta semana te invitamos a matricularte en un nuevo inicio de nuestro CURSO INTEGRAL MIEMPRESAPROPIA, que por más de 12 años ha capacitado a más de 6,700 nuevos emprendedores. Capacidad limitada. https://www.facebook.com/events/2073558702900792/", 
+          "imagen": "ideas-estudios-mercado.jpg",
+          "tipo":"Empresarial"
+      },
+      {
+          "nombre":"Taller de Coaching ",
+          "fecha": "Sábado 7 de abril", 
+          "hora": "(14:45 a 18:00)", 
+          "costo": "Gratis",
+          "ubicacion": "Sedipro UCV Lima. Alfredo Mendiola 6232- Panamericana Norte, Los Olivos, 51 Lima.", 
+          "descripcion": "En está oportunidad Daniel Vera, coach y motivador personal, será el encargado de dictar dicho taller con el que conocerás y desarrollaras tus habilidades de liderazgo y trabajo en equipo. Inscribete ya ➡ https://docs.google.com/forms/d/e/1FAIpQLScCD2ld7kRcmi31z4r1DbrVwO-LDt0FnvJcwllzxEFYw5LR4g/viewform Capacidad limitada. https://www.facebook.com/events/158539294788260/", 
+          "imagen": "tall-coaching.jpg",
+          "tipo":"Empresarial"
+      },
+      {
+          "nombre":"Wit Meetup Marzo",
+          "fecha": "Cada sábado, 7 de abril - 28 de abril", 
+          "hora": "16:00 a 20:00", 
+          "costo": "Preguntar en página",
+          "ubicacion": "CENTRO DE CAPACITACION DECCO. Calle Los Brillantes L'17 Urb. Miraflores II - etapa, 586 Castilla, Peru", 
+          "descripcion": "Curso 100% práctico Usamos plataforma moodle (material digital) Capacidad limitada. https://www.facebook.com/events/160780854583887/?active_tab=about", 
+          "imagen": "int-excel.jpg",
+          "tipo":"Empresarial"
+      }
+  ]
 
-      $(".button-collapse").sideNav();
-
-      $(".carousel.carousel-slider").carousel({
-            fullWidth: true,
-            indicators: true
-      });
-      setInterval(function(){
-            $(".carousel.carousel-slider").carousel('next');
-      }, 8000);
-
-      $.getScript("data.js");
 
 
-            
-            let containerEvents = $('.events-container')
-            for (let i = 0; i < data.length; i++) {
-                 var evento = $('#event');
+$(".button-collapse").sideNav();
 
-                  var eventsCloned = evento.clone(true);
-
-                  eventsCloned[0].childNodes[1].childNodes[1].childNodes[1].attributes[1].nodeValue = "assets/events/" + data[i].imagen;
-                  eventsCloned[0].childNodes[1].childNodes[3].children[0].textContent = data[i].nombre;
-                  eventsCloned[0].childNodes[1].childNodes[5].children[0].textContent = data[i].fecha;
-                  //  console.log(eventsCloned[0].childNodes[1].childNodes[3].children[0].textContent);
-             
-                  eventsCloned.removeClass('none');
-                  containerEvents.prepend(eventsCloned);
-
-                  eventsCloned.click(function(event) {
-                        $('.modal').modal();
-                        var modalForEvent = $('#modal1');
-                        var modalCloned = modalForEvent.clone(true);
-
-                        modalCloned[0].children[0].children[0].textContent = data[i].nombre
-                        modalCloned[0].children[0].children[1].children[0].attributes[1].nodeValue = "assets/events/" + data[i].imagen;
-                        console.log(modalCloned[0].children[0].children[1].children[0].attributes[1].nodeValue)
-
-                        // $('#modal1').remove();
-                        // containerEvents.prepend(eventsCloned);
-                        // $( "body" ).append(modalCloned.html());
-
-                        $("#modal1").replaceWith(modalCloned);
-                        $('.modal').modal();
-
-                        
-                  });
-                  
-            }
-    
-      
-
-      
-
-      // imgEven.click(function(event) {
-      //       var user = firebase.auth().currentUser;
-      //       var clonePost = structurePublication.cloneNode(true);
-      //       var inputImgmodal = document.querySelector('#input_img_post');
-      //       clonePost.querySelector('#tag-of-post').textContent = valorInputTag ;
-      //       $('#tag-of-post').attr('class', 'tags-posts');
-      //       clonePost.querySelector('#type-of-post').textContent = valorInputTypePost ;
-      //       $('#type-of-post').attr('class', 'tags-posts')
-      //       clonePost.querySelector('#description_post').textContent = inputDescriptionPostVal ;
-      //       clonePost.querySelector('#time-of-post').textContent = moment().subtract(10, 'days').calendar() + '  ' + moment().format('LT');
-        
-      //       // Cambiamos la imagen default por la imagen del usuario en los posts
-      //       $('#photo_user_post').attr('src', user.photoURL);
-      //       $('#name_user_post').text(user.displayName);
-        
-      //       // Extraemos elemento html img por defecto del post clonado
-      //       var imgPostClone = clonePost.querySelector('#img_post');
-        
-      //       // Extraemos img del input del modal
-      //       var fileImgInputNewPost = inputImgmodal.files[0].name ;
-      //       // console.log(fileImgInputNewPost);
-      //       var reader = new FileReader();
-        
-      //       reader.onload = function(event) {
-      //         // console.log(event.target.result);
-      //         imgPostClone.setAttribute('src', event.target.result);
-      //         imgPostClone.setAttribute('class', 'img-responsive');
-      //       };
-      //       reader.readAsDataURL(inputImgmodal.files[0]);
-        
-      //       clonePost.classList.remove('display_none');
-      //       containerPublications.prepend(clonePost);
-      //     });// Termina el evento de clik en  el boton submitt
-
-
-
-
-
-
-
+$(".carousel.carousel-slider").carousel({
+      fullWidth: true,
+      indicators: true
 });
+setInterval(function(){
+      $(".carousel.carousel-slider").carousel('next');
+}, 8000);
+      
+      let containerEvents = $('.events-container')
+      for (let i = 0; i < data.length; i++) {
+            var evento = $('#event');
+
+            var eventsCloned = evento.clone(true);
+
+            eventsCloned[0].childNodes[1].childNodes[1].childNodes[1].attributes[1].nodeValue = "assets/events/" + data[i].imagen;
+            eventsCloned[0].childNodes[1].childNodes[3].children[0].textContent = data[i].nombre;
+            eventsCloned[0].childNodes[1].childNodes[5].children[0].textContent = data[i].fecha;
+            //  console.log(eventsCloned[0].childNodes[1].childNodes[3].children[0].textContent);
+            
+            eventsCloned.removeClass('none');
+            containerEvents.prepend(eventsCloned);
+
+            eventsCloned.click(function(event) {
+                  $('.modal').modal();
+                  var modalForEvent = $('#modal1');
+                  var modalCloned = modalForEvent.clone(true);
+
+                  modalCloned[0].children[0].children[0].textContent = data[i].nombre
+                  modalCloned[0].children[0].children[1].children[0].attributes[1].nodeValue = "assets/events/" + data[i].imagen;
+                  // console.log(modalCloned[0].children[0].children[1].children[0].attributes[1].nodeValue)
+
+                  // $('#modal1').remove();
+                  // containerEvents.prepend(eventsCloned);
+                  // $( "body" ).append(modalCloned.html());
+
+                  $("#modal1").replaceWith(modalCloned);
+                  $('.modal').modal();
+
+                  
+            });
+            
+      }
